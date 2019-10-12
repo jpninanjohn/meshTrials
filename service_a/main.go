7b3a31c37455fa36aec1ee078bf4bed0fa036b9a
Ninan John <jninan@thoughtworks.com>
2019-10-12 10:33:21 +0530

    Add docker compose and envoy config for service a alone

---
+++ service_a/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+
+	_, _ = fmt.Fprintf(w, "Calling Service B")
+
+	// Request to service a envoy
+	req, err := http.NewRequest("GET", "http://service_a_envoy:8788/", nil)
+	if err != nil {
+		fmt.Printf("%s", err)
+	}
+
+	req.Header.Add("x-request-id", r.Header.Get("x-request-id"))
+	req.Header.Add("x-b3-traceid", r.Header.Get("x-b3-traceid"))
+	req.Header.Add("x-b3-spanid", r.Header.Get("x-b3-spanid"))
+	req.Header.Add("x-b3-parentspanid", r.Header.Get("x-b3-parentspanid"))
+	req.Header.Add("x-b3-sampled", r.Header.Get("x-b3-sampled"))
+	req.Header.Add("x-b3-flags", r.Header.Get("x-b3-flags"))
+	req.Header.Add("x-ot-span-context", r.Header.Get("x-ot-span-context"))
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+
+	if err != nil {
+		fmt.Printf("%s", err)
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Printf("%s", err)
+	}
+	_, _ = fmt.Fprintf(w, string(body))
+	_, _ = fmt.Fprintf(w, "Hello from service A")
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe(":8081", nil))
+}
